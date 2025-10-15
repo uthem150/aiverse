0e42d7570fec49e66565094572043573bbaf8df8
kim dohun <uthem150@gmail.com>
2025-10-15 21:59:14 +0900

    Reapply "feat(seo): react-snap 도입으로 동적 메타 태그 SEO 문제 해결"
    This reverts commit 810780cf5811ec4a627bb6d133c34066256b096b.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot, hydrateRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root')!;
+
+if (rootElement.hasChildNodes()) {
+  // 사전 렌더링된 HTML이 있으면 hydrateRoot를 사용
+  hydrateRoot(
+    rootElement,
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} else {
+  // 일반적인 개발 환경에서는 createRoot를 사용
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
